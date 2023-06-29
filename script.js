30debaedf176d442c3e7c48447b677e3fddbdff0
Anujraghuwanshi2408 <128280445+Anujraghuwanshi2408@users.noreply.github.com>
2023-06-29 12:33:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 //your JS code here. If required.
-const btn = document.getElementById('btn');
-btn.addEventListener('click' , toMakeDelay);
-async function toMakeDelay(){
-	const text = document.getElementById('text').value
-	const delay = document.getElementById('delay').value
-	const output = document.getElementById('output');
-	await new Promise(resolve => setTimeout(resolve , delay*1000) ) 
-	output.innerText = text;
-}
+// const btn = document.getElementById('btn');
+// btn.addEventListener('click' , toMakeDelay);
+// async function toMakeDelay(){
+// 	const text = document.getElementById('text').value
+// 	const delay = document.getElementById('delay').value
+// 	const output = document.getElementById('output');
+// 	await new Promise(resolve => setTimeout(resolve , delay*1000) ) 
+// 	output.innerText = text;
+// }
+async function displayMessage() {
+        const messageInput = document.getElementById('text');
+        const delayInput = document.getElementById('delay');
+        const outputDiv = document.getElementById('output');
+
+        const message = messageInput.value;
+        const delay = delayInput.value * 1000; // Convert seconds to milliseconds
+
+        outputDiv.textContent = 'Waiting for ' + delayInput.value + ' seconds...';
+
+        await new Promise(resolve => setTimeout(resolve, delay));
+
+        outputDiv.textContent = message;
+      }
+
+      const btn = document.getElementById('btn');
+      btn.addEventListener('click', displayMessage);
