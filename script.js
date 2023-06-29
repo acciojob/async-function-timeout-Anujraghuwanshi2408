0c7589c65b3cbe5b78e4777791b534ebd720b39b
Anujraghuwanshi2408 <128280445+Anujraghuwanshi2408@users.noreply.github.com>
2023-06-29 12:16:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const btn = document.getElementById("btn");
+btn.addEventListener("click" , toMakeDelay);
+async function toMakeDelay(){
+	const text = document.getElementById("text").value
+	const delay = document.getElementById("delay").value
+	const output = document.getElementById("delay");
+	await settimeout(() => {
+		output.innerText = text;
+	} , delay*1000)
+}
