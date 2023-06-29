7c4619df0e2a4f227c5cbb52755adb93b8058518
Anujraghuwanshi2408 <128280445+Anujraghuwanshi2408@users.noreply.github.com>
2023-06-29 12:35:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 //your JS code here. If required.
-// const btn = document.getElementById('btn');
-// btn.addEventListener('click' , toMakeDelay);
-// async function toMakeDelay(){
-// 	const text = document.getElementById('text').value
-// 	const delay = document.getElementById('delay').value
-// 	const output = document.getElementById('output');
-// 	await new Promise(resolve => setTimeout(resolve , delay*1000) ) 
-// 	output.innerText = text;
-// }
-async function displayMessage() {
-        const messageInput = document.getElementById('text');
-        const delayInput = document.getElementById('delay');
-        const outputDiv = document.getElementById('output');
+const btn = document.getElementById('btn');
+btn.addEventListener('click' , toMakeDelay);
+async function toMakeDelay(){
+	const text = document.getElementById('text').value
+	const delay = document.getElementById('delay').value
+	const output = document.getElementById('output');
+	output.innerText = '';
+	await new Promise(resolve => setTimeout(resolve , delay*1000) ) 
+	output.innerText = text;
+}
+// async function displayMessage() {
+//         const messageInput = document.getElementById('text');
+//         const delayInput = document.getElementById('delay');
+//         const outputDiv = document.getElementById('output');
 
-        const message = messageInput.value;
-        const delay = delayInput.value * 1000; // Convert seconds to milliseconds
+//         const message = messageInput.value;
+//         const delay = delayInput.value * 1000; // Convert seconds to milliseconds
 
-        outputDiv.textContent = 'Waiting for ' + delayInput.value + ' seconds...';
+//         outputDiv.textContent = 'Waiting for ' + delayInput.value + ' seconds...';
 
-        await new Promise(resolve => setTimeout(resolve, delay));
+//         await new Promise(resolve => setTimeout(resolve, delay));
 
-        outputDiv.textContent = message;
-      }
+//         outputDiv.textContent = message;
+//       }
 
-      const btn = document.getElementById('btn');
-      btn.addEventListener('click', displayMessage);
+//       const btn = document.getElementById('btn');
+//       btn.addEventListener('click', displayMessage);
