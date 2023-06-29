bfe8035b076fa113ae4a5d378dcdc333c91d3fbb
Anujraghuwanshi2408 <128280445+Anujraghuwanshi2408@users.noreply.github.com>
2023-06-29 12:32:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //your JS code here. If required.
-const btn = document.getElementById("btn");
-btn.addEventListener("click" , toMakeDelay);
+const btn = document.getElementById('btn');
+btn.addEventListener('click' , toMakeDelay);
 async function toMakeDelay(){
-	const text = document.getElementById("text").value
-	const delay = document.getElementById("delay").value
-	const output = document.getElementById("output");
+	const text = document.getElementById('text').value
+	const delay = document.getElementById('delay').value
+	const output = document.getElementById('output');
 	await new Promise(resolve => setTimeout(resolve , delay*1000) ) 
 	output.innerText = text;
 }
