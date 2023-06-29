1e883cf54c90a7114a5cb70c2e56520c0a5913f0
Anujraghuwanshi2408 <128280445+Anujraghuwanshi2408@users.noreply.github.com>
2023-06-29 12:42:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ async function toMakeDelay(){
 	const text = document.getElementById('text').value
 	const delay = document.getElementById('delay').value
 	const output = document.getElementById('output');
-	output.innerText = ' ';
-	await new Promise(resolve => setTimeout(resolve , delay*1000) ) 
 	output.innerText = text;
+	await new Promise(resolve => setTimeout(resolve , delay*1000) ) 
+	output.innerText = '';
+	
 }
 // async function displayMessage() {
 //         const messageInput = document.getElementById('text');
